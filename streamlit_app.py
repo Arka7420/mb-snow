59c78b52baa343b0133d02b2e4ad51868e0acfea
Arka7420 <136488445+Arka7420@users.noreply.github.com>
2023-06-15 13:00:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit
-#streamlit.title('My Parents New Healthy Dinner')
-#streamlit.header('Breakfast Menu')
-#streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-#streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
-#streamlit.text('🐔 🥑🍞Hard-Boiled Free-Range Egg')
+streamlit.title('My Parents New Healthy Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 🥑🍞Hard-Boiled Free-Range Egg')
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
