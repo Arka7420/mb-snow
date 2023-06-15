bc6ace4574fac2f663e8bcad9d788ff40ec8e967
Arka7420 <136488445+Arka7420@users.noreply.github.com>
2023-06-15 14:50:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,14 +10,3 @@ streamlit.dataframe(my_fruit_list)
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
-# Display the table on the page.
-my_fruit_list = my_fruit_list.set_index('Fruit')
-# Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avacado",'Strawberries'])
-select GRADER(step, (actual = expected), actual, expected, description) as graded_results from (
-  SELECT 'DORA_IS_WORKING' as step
- ,(select 223) as actual
- , 223 as expected
- ,'Dora is working!' as description
-); 
